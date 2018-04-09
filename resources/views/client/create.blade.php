af1dfcd26886fcd87a017794da885666339bff76
Abdelhady <ag1160@fayoum.edu.eg>
2018-04-09 22:42:49 +0200

    adding the assets in public folder

--- resources/views/client/create.blade.php
+++ resources/views/client/create.blade.php
@@ -1,8 +1,9 @@
-create
-<?php
-/**
- * Created by PhpStorm.
- * User: abdelhady
- * Date: 07/04/2018
- * Time: 01:01 ص
- */
+
+@extends('layouts.app')
+
+@section('content')
+
+    create
+
+@endsection
+
